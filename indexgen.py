b0b05fbc2cd69c3943032cd571e87b8b5cde9c04
expiredlastweek <minimizeentropy69@gmail.com>
2021-12-29 12:42:00 +0800

    fix link gen error

--- indexgen.py
+++ indexgen.py
@@ -1,9 +1,11 @@
+# write in markdown and convert to html with script
+
 import os
 
 index_fn = "./index.html"
 
-header = "<h1>hello world</h1>"
-wilt_header = "<p>what i've learned today (basically tweeting in private):</p>"
+header = "<h1>minds all the way down</h1>"
+wilt_header = "<h2>what i've learned today (basically tweeting in private):</h2>"
 
 f = open(index_fn, "w")
 
@@ -16,6 +18,6 @@ for filename in os.listdir('./wilt'):
 wilt_list.sort(reverse=True)
 
 for i in wilt_list:
-	f.write(f"<a href=\"{i}\">{i[:-5]}</a>\n")
+	f.write(f"<a href=\"./wilt/{i}\">{i[:-5]}</a>\n")
 
 f.close()
